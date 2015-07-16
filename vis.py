d2a7cf5ed947a8757b3ffd384b51e24acd3db5f4
Dominik Stańczak <stanczakdominik@gmail.com>
2015-07-16 23:37:07 +0200

    cos dziala

--- vis.py
+++ vis.py
@@ -33,33 +33,33 @@ def localAction(point, radius=0.0001):
     indices_in_radius = (r_distances**2+z_distances**2<radius**2)
     number_points_inside_radius=np.count_nonzero(indices_in_radius)
     # print(indices_in_radius)
-    print(number_points_inside_radius)
+    # print(number_points_inside_radius)
 
-    fig = plt.gcf()
-    plt.quiver(r[indices_in_radius],z[indices_in_radius],vr[indices_in_radius],vz[indices_in_radius], label="Input data")
-    plt.scatter(r[indices_in_radius],z[indices_in_radius], label="Input data", alpha=0.05)
-    plt.scatter(r[indices_in_radius],z[indices_in_radius],
-        label="Input data in circle", alpha=0.5)
-    plt.plot(point[0], point[1], "bo-", label="plotted point" )
-    plt.xlabel("r")
-    plt.ylabel("z")
-    fig.gca().add_artist(plt.Circle(point, radius, color='g', alpha=0.5))
-    plt.grid()
-    plt.legend()
-    plt.axes().set_aspect('equal', 'datalim')
-    plt.xlim(point[0]-2*radius, point[0]+2*radius)
-    plt.ylim(point[1]-2*radius, point[1]+2*radius)
-    plt.show()
+    # fig = plt.gcf()
+    # plt.quiver(r[indices_in_radius],z[indices_in_radius],vr[indices_in_radius],vz[indices_in_radius], label="Input data")
+    # plt.scatter(r[indices_in_radius],z[indices_in_radius], label="Input data", alpha=0.05)
+    # plt.scatter(r[indices_in_radius],z[indices_in_radius],
+    #     label="Input data in circle", alpha=0.5)
+    # plt.plot(point[0], point[1], "bo-", label="plotted point" )
+    # plt.xlabel("r")
+    # plt.ylabel("z")
+    # fig.gca().add_artist(plt.Circle(point, radius, color='g', alpha=0.5))
+    # plt.grid()
+    # plt.legend()
+    # plt.axes().set_aspect('equal', 'datalim')
+    # plt.xlim(point[0]-2*radius, point[0]+2*radius)
+    # plt.ylim(point[1]-2*radius, point[1]+2*radius)
+    # plt.show()
 
     iterations=0
 
     if(iterations>10):
-        return radius
+        return np.array([vr_interpolated, vz_interpolated]), radius
     if(number_points_inside_radius>8):
-        localAction(point, radius*0.9)
+        return localAction(point, radius*0.9)
         iterations+=1
     elif(number_points_inside_radius<8):
-        localAction(point, radius*1.055)
+        return localAction(point, radius*1.055)
         iterations+=1
     else:
         r_distances_inside=r_distances[indices_in_radius]
@@ -70,30 +70,44 @@ def localAction(point, radius=0.0001):
         weight_sum=np.sum(weights)
         vr_interpolated=np.sum(weights*r_velocities_inside)/weight_sum
         vz_interpolated=np.sum(weights*z_velocities_inside)/weight_sum
-        print(vr_interpolated, vz_interpolated)
-        print(r_velocities_inside)
-        print(z_velocities_inside)
-        fig = plt.gcf()
-        plt.quiver(r[indices_in_radius],z[indices_in_radius],vr[indices_in_radius],
-            vz[indices_in_radius], label="Input data")
-        plt.quiver(point[0],point[1],vr_interpolated,vz_interpolated,
-            label="Found point")
-        #plt.scatter(r,z, label="Input data", alpha=0.05)
-        plt.scatter(r[indices_in_radius],z[indices_in_radius],
-            label="Input data in circle", alpha=0.5)
-        plt.plot(point[0], point[1], "bo-", label="plotted point" )
-        plt.xlabel("r")
-        plt.ylabel("z")
-        fig.gca().add_artist(plt.Circle(point, radius, color='g', alpha=0.5))
-        plt.grid()
-        plt.legend()
-        plt.axes().set_aspect('equal', 'datalim')
-        plt.xlim(point[0]-2*radius, point[0]+2*radius)
-        plt.ylim(point[1]-2*radius, point[1]+2*radius)
-        plt.show()
+        # print(vr_interpolated, vz_interpolated)
+        # print(r_velocities_inside)
+        # print(z_velocities_inside)
+        # fig = plt.gcf()
+        # plt.quiver(r[indices_in_radius],z[indices_in_radius],vr[indices_in_radius],
+        #     vz[indices_in_radius], label="Input data")
+        # plt.quiver(point[0],point[1],vr_interpolated,vz_interpolated,
+        #     label="Found point")
+        # #plt.scatter(r,z, label="Input data", alpha=0.05)
+        # plt.scatter(r[indices_in_radius],z[indices_in_radius],
+        #     label="Input data in circle", alpha=0.5)
+        # plt.plot(point[0], point[1], "bo-", label="plotted point" )
+        # plt.xlabel("r")
+        # plt.ylabel("z")
+        # fig.gca().add_artist(plt.Circle(point, radius, color='g', alpha=0.5))
+        # plt.grid()
+        # plt.legend()
+        # plt.axes().set_aspect('equal', 'datalim')
+        # plt.xlim(point[0]-2*radius, point[0]+2*radius)
+        # plt.ylim(point[1]-2*radius, point[1]+2*radius)
+        # plt.show()
+        # print (np.array([vr_interpolated, vz_interpolated]), radius)
+        # print("REACHED")
+        # return (vr_interpolated, vz_interpolated, radius)
+        return np.array([vr_interpolated, vz_interpolated]), radius
 
-        return np.array([vr_interpolated, vz_interpolated])
-
+def step_to_next_point(points):
+    last_point=points[-1]
+    interpolation = localAction(last_point)
+    # print("last point", last_point)
+    # print("this",interpolation)
+    local_velocities, step=localAction(last_point)
+    shift=step*local_velocities/np.sqrt(np.sum(local_velocities**2))
+    new_point = last_point + shift
+    # print("diff", new_point-last_point)
+    points=np.vstack((points,new_point))
+    # print(new_point)
+    return points
 
 #read data
 B4LINE = np.loadtxt("B4LINE.dat")
@@ -103,22 +117,48 @@ z = B4LINE[:,0]
 r = B4LINE[:,1]
 vr = B4LINE[:,2]
 vz = B4LINE[:,3]
+
+fig = plt.gcf()
 plt.quiver(r,z,vr,vz, label="Input data")
-plt.scatter(r,z, label="Input data", alpha=0.05)
 
-point = np.array([0.00068850, 0.003625])
+StartingZ=np.linspace(min(z)*0.99,max(z)*0.99,100)
+StartingZ=np.vstack(((np.ones_like(StartingZ)*(max(r)-min(r))/100000+min(r)).T, StartingZ.T)).T
 
-# StartingZ=np.linspace(min(z),max(z),100)
-# #StartingZ=np.hstack((StartingZ, np.zeros_like(StartingZ)))
-# plt.plot(np.zeros_like(StartingZ), StartingZ, label="Starting data")
+def fieldline(points):
+    print(points[-1])
+    i=0
+    while(points[-1,0] < max(r) and points[-1,0]>min(r) and points[-1,1]<max(z) and points[-1,1]>min(z)):
+        i+=1
+        # print(str(i) +"\t", end="")
+        points=step_to_next_point(points)
+    plt.plot(points[:,0], points[:,1], "b-")
+#plt.scatter(r,z, label="Input data", alpha=0.05)
+#plt.scatter(r,z,
+#    label="Input data in circle", alpha=0.5)
+fieldline(np.array([[0.000812395, 0.00130208]]))
+# for linenumber in range(len(StartingZ)):
+#     Array=np.array([[StartingZ[linenumber,0], StartingZ[linenumber,1]]])
+#     fieldline(Array)
 
-
-
+fieldline(np.array([[0.00052395, 0.0000208]]))
+# fieldline(np.array([[0.005, -0.003]]))
+fieldline(np.array([[0.001, 0.002]]))
+# fieldline(np.array([[0.001, 0.0002]]))
+# fieldline(np.array([[0.001, 0.0001]]))
+# fieldline(np.array([[0.001, 0.001]]))
+# fieldline(np.array([[0.001, -0.0001]]))
+fieldline(np.array([[0.001, 0.004]]))
+fieldline(np.array([[0.001, 0.003]]))
+fieldline(np.array([[0.001, -0.004]]))
+fieldline(np.array([[0.001, -0.005]]))
+fieldline(np.array([[0.001, -0.003]]))
+fieldline(np.array([[0.001, -0.002]]))
+# fieldline(np.array([[0.001, -0.001]]))
 plt.xlabel("r")
 plt.ylabel("z")
 plt.grid()
 plt.legend()
-# plt.show()
-plt.clf()
-
-localAction(point)
+plt.xlim(min(r),max(r))
+plt.ylim(min(z),max(z))
+plt.savefig("line.png")
+plt.show()
